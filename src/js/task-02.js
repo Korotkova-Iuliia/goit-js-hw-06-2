024d812a10d5a84c9db7845638386c2f5837f107
Korotkova-Iuliia <3589900@gmail.com>
2025-09-26 15:11:29 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientList = document.querySelector('#ingredients');
+
+const ingredientItem = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+  (li.textContent = ingredient), console.log(ingredient);
+  return li;
+});
+ingredientList.append(...ingredientItem);
