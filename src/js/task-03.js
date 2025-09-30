5b9c2391c0de9afc2a91606d3a8ab9cb990a2514
Korotkova-Iuliia <3589900@gmail.com>
2025-09-30 14:49:03 +0200

    task03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -11,24 +11,72 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
 ];
 console.log(images);
 const galleryEl = document.querySelector('.gallery');
-galleryEl.style.backgroundColor = 'teal';
-galleryEl.style.display = 'flex';
-galleryEl.style.flexWrap = 'wrap';
-galleryEl.style.justifyContent = 'center';
-galleryEl.style.alignItems = 'center';
-galleryEl.style.padding = '0';
-
 const galleryItem = images
   .map(({ url, alt }) => {
     return `
-  <li style="width: 540px;" >
-  <img src="${url}" alt="${alt}" style="max-width: 100%; padding: 40px;">
+  <li class="gallery_item">
+  <img class="gallery_img" src="${url}" alt="${alt}" >
     </li>
   `;
   })
   .join('');
 
 galleryEl.insertAdjacentHTML('afterbegin', galleryItem);
+// const galleryEl = document.querySelector('.gallery');
+// galleryEl.style.backgroundColor = 'teal';
+// galleryEl.style.display = 'flex';
+// galleryEl.style.flexWrap = 'wrap';
+// galleryEl.style.justifyContent = 'center';
+// galleryEl.style.alignItems = 'center';
+// galleryEl.style.padding = '0';
+
+// const galleryItem = images
+//   .map(({ url, alt }) => {
+//     return `
+//   <li style="width: 540px;" >
+//   <img src="${url}" alt="${alt}" style="max-width: 100%; padding: 40px;">
+//     </li>
+//   `;
+//   })
+//   .join('');
+
+// galleryEl.insertAdjacentHTML('afterbegin', galleryItem);
