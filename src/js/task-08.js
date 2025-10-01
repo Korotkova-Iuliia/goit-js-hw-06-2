add44e3b78ce393928c2685b546d7a4d5d3045c2
Korotkova-Iuliia <3589900@gmail.com>
2025-10-01 19:23:31 +0200

    task08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const loginFormEl = document.querySelector('.login-form');
 
+console.log(loginFormEl);
+
+loginFormEl.addEventListener('submit', onLoginSubmit);
+
+function onLoginSubmit(e) {
+  e.preventDefault();
+  const form = e.currentTarget.elements;
+
+  const email = form.email.value;
+  const password = form.password.value;
+  if (email === '' || password === '') {
+    alert(` Всі поля повинні бути заповнені`);
+  }
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+
+  form.reset();
+}
